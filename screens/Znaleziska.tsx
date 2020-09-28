cb38c93e33b40095c3e727a793a1918b9b9f5995
Pawel Bala <pawel3ala@gmail.com>
2020-09-28 17:32:30 +0200

    pagination

--- screens/Znaleziska.tsx
+++ screens/Znaleziska.tsx
@@ -4,9 +4,44 @@ import { useNavigation } from '@react-navigation/native';
 import { Text, View } from '../components/Themed';
 import { links } from '../constants/FakeData'
 import Link from '../components/Link'
+import { useState, useEffect } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import axios from 'axios'
+import md5 from "react-native-md5";
 
 
 const Znaleziska = (props) => {
+
+    const config = {
+        appKey: 'XVnNJ8CwBc',
+        appSecret: 'K2y2UpZTCo'
+    }
+
+    const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+    
+    const fetchData = (page) => {
+
+        const url = `https://a2.wykop.pl/Links/Promoted/page/${page}/appkey/XVnNJ8CwBc/`
+
+        axios.defaults.headers.common['apisign'] = md5.hex_md5(`${config.appSecret}${url}`);
+        axios.post(url)
+            .then((response) => {
+                const { data: { data: dataToBeLoaded } } = response
+                setData([...data, ...dataToBeLoaded])
+            })
+            .catch((err) => console.error(err))
+    }
+
+    const loadMoreData = () => {
+        setPage(prvPage => prvPage + 1)
+    }
+
+    // TODO: Load only what I need
+    // TDOD: figure out why useFocusEffect does not work
+    useEffect(() => {
+        fetchData(page)
+    }, [page])
 
     const renderItem = ({ item }) => {
         return (
@@ -20,11 +55,13 @@ const Znaleziska = (props) => {
     }
 
     return (
-            <FlatList
-                data={links}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-            />
+        <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.title}
+            onEndReachedThreshold={0.9}
+            onEndReached={loadMoreData}
+        />
     )
 }
 
