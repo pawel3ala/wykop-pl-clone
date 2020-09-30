6b4670e170a8eeb9a7f5f6bb9a2434abbe8f86e8
Pawel Bala <pawel@Pawels-MacBook-Pro.local>
2020-09-30 11:19:05 +0200

    reducing the amount of fetched data

--- screens/Znaleziska.tsx
+++ screens/Znaleziska.tsx
@@ -7,28 +7,36 @@ import { useFocusEffect } from '@react-navigation/native';
 import axios from 'axios'
 import md5 from "react-native-md5";
 
+interface IZnalezisko {
+    id: number,
+    title: string,
+    preview: string,
+    comments_count: number,
+    source_url: string,
+    vote_count: number
+}
 
-const Znaleziska = (props) => {
+const Znaleziska = () => {
 
     const config = {
         appKey: 'XVnNJ8CwBc',
         appSecret: 'K2y2UpZTCo'
     }
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState<IZnalezisko[]>([])
     const [page, setPage] = useState(1)
     const navigation = useNavigation();
 
-
-    const fetchData = (page) => {
+    const fetchData = (page: number) => {
 
         const url = `https://a2.wykop.pl/Links/Promoted/page/${page}/appkey/XVnNJ8CwBc/`
-
         axios.defaults.headers.common['apisign'] = md5.hex_md5(`${config.appSecret}${url}`);
         axios.post(url)
             .then((response) => {
-                const { data: { data: dataToBeLoaded } } = response
-                setData([...data, ...dataToBeLoaded])
+                const { data: { data: fetchedData } } = response
+                // Extracting the minimum amount of data to be held in component
+                const dataToBeSaved = fetchedData.map(item => (({ id, title, preview, comments_count, source_url, vote_count }) => ({ id, title, preview, comments_count, source_url, vote_count}))(item))
+                setData([...data, ...dataToBeSaved])
             })
             .catch((err) => console.error(err))
     }
@@ -37,37 +45,35 @@ const Znaleziska = (props) => {
         setPage(prvPage => prvPage + 1)
     }
 
-    // TODO: Load only what I need
     // TDOD: figure out why useFocusEffect does not work
     useEffect(() => {
         fetchData(page)
     }, [page])
 
     const renderItem = ({ item }) => {
-        const { title, preview, vote_count, comments_count, id, related_count, bury_count, tags } = item
-        const objToPass = { title, preview, vote_count, comments_count, id, related_count, bury_count, tags }
+        const { title, preview, vote_count, comments_count, id } = item
+
+        return (
+            <TouchableHighlight onPress={() => navigation.navigate('ZnaleziskoScreen', {id: id})}>
+                <Link
+                    title={title}
+                    preview={preview}
+                    vote_count={vote_count}
+                    comments_count={comments_count}
+                />
+            </TouchableHighlight>
+        )
+    }
 
     return (
-        <TouchableHighlight onPress={() => navigation.navigate('ZnaleziskoScreen', objToPass)}>
-            <Link
-                title={title}
-                preview={preview}
-                vote_count={vote_count}
-                comments_count={comments_count}
-            />
-        </TouchableHighlight>
+        <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.title}
+            onEndReachedThreshold={0.9}
+            onEndReached={loadMoreData}
+        />
     )
-}
-
-return (
-    <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.title}
-        onEndReachedThreshold={0.9}
-        onEndReached={loadMoreData}
-    />
-)
 }
 
 export default Znaleziska
